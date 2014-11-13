1be17684ae96000d3f9d89ea1739785601c92fe4
Jacob B <barrow.jacob@gmail.com>
2014-11-13 20:56:39 +0000

    Not complete...

--- strulo_04.py
+++ strulo_04.py
@@ -4,22 +4,30 @@ Strulo 04 - Cracking a Cipher
 
 import collections
 import string 
-
+path_to_english_file = '/Users/jacob/Desktop/Python/Strulo/english.txt'
 while True:
 	alphabet = string.ascii_lowercase
 	# a)
-	cipher = input('Enter cipher to crack\n>> ')
-	cipher = ''.join([l for l in cipher if l in alphabet])
-	letters = collections.Counter(cipher)
+	plain_file = ''
+	with open(path_to_english_file) as file:
+		for word in file:  
+			for c in word:
+				c = c.lower() 
+				if c in alphabet:
+					plain_file += c
+	letters = collections.Counter(plain_file)
+	print(letters)
 	# print(letters)
 	
 	# b)
+	cipher = input('Enter cipher to crack\n>> ')
 	keyword = ''.join(l for l in list(letters.keys()))
 	key = ''.join([l for l in alphabet if l not in keyword])
 	key = keyword + key 
 	# print(key)
 	output = ''
 	for l in cipher:
-		i = alphabet.index(l)
-		output += key[i]
+		if l in alphabet:
+			i = alphabet.index(l)
+			output += key[i]
 	print(output)
