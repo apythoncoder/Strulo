9809a310dfb4ebae75707b1daff408e08054041a
Jacob B <barrow.jacob@gmail.com>
2014-11-13 20:29:31 +0000

    04 Complete?

--- strulo_04.py
+++ strulo_04.py
@@ -6,8 +6,20 @@ import collections
 import string 
 
 while True:
+	alphabet = string.ascii_lowercase
+	# a)
 	cipher = input('Enter cipher to crack\n>> ')
-	cipher = ''.join([l for l in cipher if l in string.ascii_lowercase])
+	cipher = ''.join([l for l in cipher if l in alphabet])
 	letters = collections.Counter(cipher)
-	print(letters)
+	# print(letters)
 	
+	# b)
+	keyword = ''.join(l for l in list(letters.keys()))
+	key = ''.join([l for l in alphabet if l not in keyword])
+	key = keyword + key 
+	# print(key)
+	output = ''
+	for l in cipher:
+		i = alphabet.index(l)
+		output += key[i]
+	print(output)
